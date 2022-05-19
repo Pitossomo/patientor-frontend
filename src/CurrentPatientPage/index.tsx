c3e6e7491731e2efd4f7c0534ff0cc6aeffac31e
pitossomo <pitossomo@gmail.com>
2022-05-19 06:36:29 -0300

    Fetch data from backend for the single patient page, lacks persistance in global state (only stored in local state by now)

--- src/CurrentPatientPage/index.tsx
+++ src/CurrentPatientPage/index.tsx
@@ -1,26 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Typography } from "@material-ui/core";
 
 import HealthRatingBar from "../components/HealthRatingBar";
 import { useParams } from "react-router-dom";
 import GenderIcon from "../components/GenderIcon";
-// import GenderIcon from "../components/GenderIcon";
+import axios from "axios";
+import { apiBaseUrl } from "../constants";
+import { Patient } from "../types";
 
 const PatientPage = () => {
   const { id } = useParams<{ id: string }>();
+  const [currentPatient, setCurrentPatient] = useState<Patient | undefined>(undefined);
   console.log(id);
-  // const [{ currentPatient }, dispatch] = useStateValue();
-  // const [error, setError] = React.useState<string>();
-  const currentPatient = {
-    "id": "d2773822-f723-11e9-8f0b-362b9e155667",
-    "name": "Dana Scully",
-    "dateOfBirth": "1974-01-05",
-    "ssn": "050174-432N",
-    "gender": "female",
-    "occupation": "Forensic Pathologist"
-  };
 
-  return (
+  if (!id) return null;
+
+  React.useEffect(() => {
+    void axios.get<void>(`${apiBaseUrl}/ping`).then(result => console.log(result.data));
+
+    const fetchPatient = async () => {
+      try {
+        const { data: patientFromApi } = await axios.get<Patient>(
+          `${apiBaseUrl}/patients/${id}`
+        );
+
+        console.log(patientFromApi);
+
+        setCurrentPatient(patientFromApi);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    void fetchPatient();
+  }, []);
+
+  // const [error, setError] = React.useState<string>();
+
+  if (currentPatient) return (
     <div className="App">
       <Box>
         <Typography variant="h6">
@@ -32,12 +48,13 @@ const PatientPage = () => {
           ? <Typography>occupation: {currentPatient.occupation}</Typography>
           : null
         }
-
       </Box>
 
       <HealthRatingBar showText={false} rating={1} />
     </div>
   );
+
+  else return null;
 };
 
 export default PatientPage;
