e1c09128dcde42c86acd84503145e5e2d9cf3adc
pitossomo <pitossomo@gmail.com>
2022-05-26 05:44:49 -0300

    Add types for Entries Ex 9.19

--- src/types.ts
+++ src/types.ts
@@ -18,3 +18,47 @@ export interface Patient {
   ssn?: string;
   dateOfBirth?: string;
 }
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthCareEntry extends BaseEntry {
+  type: "OccupationalHealthcare",
+  employerName: string;
+  sickLeave: {
+    startDate: string,
+    endDate: string
+  };
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string,
+    criteria: string
+  };
+}
+
+export type Entry = HospitalEntry | OccupationalHealthCareEntry | HealthCheckEntry;
+
+// Special omit for unions
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown ? Omit<T, K> : never;
+// Entry without id property
+export type EntryWithoutId = UnionOmit<Entry, 'id'>;
